bca65f3640a442413768cea93f41d43028e2c715
natewu <36091727+natewu@users.noreply.github.com>
2020-10-17 16:46:23 -0600

    transfer

--- src/GUI.java
+++ src/GUI.java
@@ -13,71 +13,101 @@ public class GUI{
     public String user;
     public String pass;
     public static boolean isLoggedIn = false;
+    boolean active = false;
+
+    JFrame window = new JFrame("Digital Payment Services");
+    JPanel login = new JPanel();
+    JPanel banking = new JPanel();
+    JPanel transfer = new JPanel();
+
+    JTextField userField = new JTextField();
+    JTextField passField = new JTextField();
+    JButton loginButton = new JButton("Login");
+    JButton registerButton = new JButton("Register");
+
+    JTextField amountField = new JTextField();
+    JButton deposit = new JButton("Deposit");
+    JButton withdraw = new JButton("Withdraw");
+    JButton send = new JButton("Transfer Money");
+    JLabel amount = new JLabel("$");
+    JLabel balanceLabel = new JLabel("$0");
+    JLabel warnings = new JLabel();
+
+    JTextField idField = new JTextField();
+    JButton sendFunds = new JButton("Transfer");
 
     void frame(){
         //Initialization events
 
-        JFrame window = new JFrame("Digital Payment Services");
-        JPanel login = new JPanel();
-        JPanel banking = new JPanel();
-
-        JTextField user = new JTextField();
-        JTextField pass = new JTextField();
-        JTextField amountField = new JTextField();
-        JButton loginButton = new JButton("Login");
-        JButton registerButton = new JButton("Register");
-        JButton deposit = new JButton("Deposit");
-        JButton withdraw = new JButton("Withdraw");
-        JLabel amount = new JLabel("$");
-        JLabel balanceLabel = new JLabel("$0");
-        JLabel warnings = new JLabel();
-        int centerX = 500;
+        
+        int centerX = 400;
         int centerY = 550;
 
-        user.setBounds(centerX/2 - 200/2,100,200,25);
-        pass.setBounds(centerX/2 - 200/2,130,200,25);
+        //Login Panel
+        
+        userField.setBounds(centerX/2 - 200/2,100,200,25);
+        passField.setBounds(centerX/2 - 200/2,130,200,25);
         loginButton.setBounds(centerX/2 - 100/2 - 60,200,100,50);
         registerButton.setBounds(centerX/2 - 100/2 + 60,200,100,50);
 
         login.setBounds(0, 0, centerX, centerY);
         login.setBackground(Color.lightGray);
         login.setLayout(null);
-        login.add(user);
+        login.add(userField);
         login.add(loginButton);
-        login.add(pass);
+        login.add(passField);
         login.add(registerButton);
         login.setVisible(true);
         
-        deposit.setBounds(centerX/2 - 100/2 + 60,200,100,50);
-        withdraw.setBounds(centerX/2 - 100/2 - 60,200,100,50);
+        //Banking Panel
+        
+        deposit.setBounds(centerX/2 - 105/2 + 55,200,105,50);
+        withdraw.setBounds(centerX/2 - 105/2 - 55,200,105,50);
+        send.setBounds(centerX/2 - 215/2,260,215,50);
         amountField.setBounds(centerX/2 - 200/2,100,200,25);
         amount.setBounds(centerX/2 - 200/2 - 25,100,100,25);
         balanceLabel.setBounds(centerX/2 - 100/2,10,100,25);
-        warnings.setBounds(centerX/2 - 200/2,70,250,25);
+        warnings.setBounds(centerX/2 - 200/2,70,300,25);
 
         banking.setBounds(0, 0, centerX, centerY);
         banking.setBackground(Color.lightGray);
         banking.setLayout(null);
+        banking.setVisible(true);
         banking.add(deposit);
         banking.add(withdraw);
+        banking.add(send);
         banking.add(amountField);
         banking.add(amount);
         banking.add(balanceLabel);
         banking.add(warnings);
-        banking.setVisible(true);
+        
 
+        //Transfer Panel
+       
+        idField.setBounds(centerX/2 - 200/2,130,200,25);
+        sendFunds.setBounds(centerX/2 - 100/2,200,100,50);
+        transfer.setBounds(0, 0, centerX, centerY);
+        transfer.setBackground(Color.lightGray);
+        transfer.setLayout(null);
+        transfer.setVisible(true);
+
+        transfer.add(sendFunds);
+        transfer.add(idField);        
+
+        //Main Frame
         window.setSize(centerX, centerY);
         window.setLocationRelativeTo(null);
         window.setLayout(null);
         window.add(login);
+        window.setResizable(false);
         window.setVisible(true);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         registerButton.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
-                fileUser.writeFile(user.getText(), fileUser.userList);
-                filePass.writeFile(pass.getText(), filePass.passList);
-                Security.updateIndex(user.getText());
+                fileUser.writeFile(userField.getText(), fileUser.userList);
+                filePass.writeFile(passField.getText(), filePass.passList);
+                Security.updateIndex(userField.getText());
                 System.out.println(Security.userIndex);
             }
         });
@@ -85,17 +115,17 @@ public class GUI{
             public void actionPerformed(ActionEvent e){
                 fileUser.readFile(fileUser.userList);
                 filePass.readFile(filePass.passList);
-                Security.password(user.getText(), pass.getText());
+                Security.password(userField.getText(), passField.getText());
                 if(isLoggedIn==true){
                     global.balance = "./balances/balance"+Security.userIndex+".txt";
                     try{
-                        UpdateStats.updateBalance();
+                        UpdateStats.refreshBalance();
                         global.readBalance(global.balance);
                         balanceLabel.setText("$"+global.balanceGlobal[0]); 
                     }
                     catch(ArrayIndexOutOfBoundsException E){
                         global.writeFile("0", global.balance);
-                        UpdateStats.updateBalance();
+                        UpdateStats.refreshBalance();
                         global.readBalance(global.balance);
                     }
                     window.remove(login);
@@ -113,7 +143,7 @@ public class GUI{
                         String tempBal = Double.toString(Double.parseDouble(global.balanceGlobal[0]) + Double.parseDouble(amountField.getText()));
                         global.writeBalance(Security.userIndex, tempBal, global.balance);
                         System.out.println(global.balance);
-                        UpdateStats.updateBalance();
+                        UpdateStats.refreshBalance();
                         balanceLabel.setText("$"+tempBal);
                         warnings.setText("");
                     }
@@ -130,16 +160,19 @@ public class GUI{
         withdraw.addActionListener(new ActionListener(){
             public void actionPerformed(ActionEvent e){
                 try{
-                    if(Double.parseDouble(amountField.getText())>0){
+                    if(Double.parseDouble(amountField.getText())>0 && Double.parseDouble(amountField.getText()) - Double.parseDouble(global.balanceGlobal[0])<=0){
                         String tempBal = Double.toString(Double.parseDouble(global.balanceGlobal[0]) - Double.parseDouble(amountField.getText()));
                         global.writeBalance(Security.userIndex, tempBal, global.balance);
                         System.out.println(global.balance);
-                        UpdateStats.updateBalance();
+                        UpdateStats.refreshBalance();
                         balanceLabel.setText("$"+tempBal);
                         warnings.setText("");
                     }
-                    else{
+                    else if(Double.parseDouble(amountField.getText()) - Double.parseDouble(global.balanceGlobal[0])<=0){
                         warnings.setText("Error: Withdraws must be greater than $0.");
+                    }
+                    else{
+                        warnings.setText("Error: Withdraw greater than available funds.");
                     }
                 }
                 catch(NumberFormatException n){
@@ -148,9 +181,70 @@ public class GUI{
                 window.repaint();
             }
         });
+        send.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                if(active==false){
+                    window.remove(banking);
+                    window.repaint();
+                    transfer.add(send);
+                    transfer.add(amount);
+                    transfer.add(amountField);
+                    transfer.add(balanceLabel);
+                    transfer.add(warnings);
+                    send.setText("Back");
+                    send.setBounds(centerX/2 - 100/2,260,100,50);
+                    window.add(transfer);
+                    window.validate();
+                    active = true;
+                }
+                else if(active == true){
+                    window.remove(transfer);
+                    window.repaint();
+                    banking.add(send);
+                    banking.add(amount);
+                    banking.add(amountField);
+                    banking.add(balanceLabel);
+                    banking.add(warnings);
+                    send.setText("Transfer Money");
+                    send.setBounds(centerX/2 - 215/2,260,215,50);
+                    window.add(banking);
+                    window.validate();
+                    active = false;
+                }
+                
+            }
+        });
+        sendFunds.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                updateBalance(Integer.parseInt(idField.getText()), "withdraw");
+            }
+        });
 
-        this.user = user.getText();
-        this.pass = pass.getText();
+        this.user = userField.getText();
+        this.pass = passField.getText();
+    }
+    
+    void updateBalance(int id, String type){
+        try{
+            if(Double.parseDouble(amountField.getText())>0 && Double.parseDouble(amountField.getText()) - Double.parseDouble(global.balanceGlobal[0])<=0){
+                String tempBal = Double.toString(Double.parseDouble(global.balanceGlobal[0]) - Double.parseDouble(amountField.getText()));
+                global.writeBalance("./balances/balance"+id+".txt", tempBal, global.balance);
+                System.out.println(global.balance);
+                UpdateStats.refreshBalance();
+                balanceLabel.setText("$"+tempBal);
+                warnings.setText("");
+            }
+            else if(Double.parseDouble(amountField.getText()) - Double.parseDouble(global.balanceGlobal[0])<=0){
+                warnings.setText("Error: "+type+"s must be greater than $0.");
+            }
+            else{
+                warnings.setText("Error: "+type+"s greater than available funds.");
+            }
+        }
+        catch(NumberFormatException n){
+            warnings.setText("Error: "+type+"s must an integer.");
+        }
+        window.repaint();
     }
 }
 class Security{
@@ -177,7 +271,7 @@ class Security{
     }
 }
 class UpdateStats{
-    static void updateBalance(){
+    static void refreshBalance(){
         try{
             FileRW updateBal = new FileRW();
             updateBal.readFile(GUI.global.balance);
@@ -188,7 +282,7 @@ class UpdateStats{
             GUI.global.balanceGlobal[0] = updateBal.output[0];
         }
         catch(Exception e){
-
         }
     }
+    
 }
